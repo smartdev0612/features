b161ede7e6d25908016c243c8b0bc7e029c6c60e
smartdev0612 <danieldev0612@gmail.com>
2022-11-11 17:14:14 -0800

    test main annotation and inference

---
+++ arrays.ts
@@ -0,0 +1,24 @@
+const carMakers = ['ford', 'toyota', "chevy"];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+  ['f150'],
+  ['corol'],
+  ['camaro']
+]
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+})
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10'];
+importantDates.push('2020-10-10');
---
+++ types.ts
@@ -0,0 +1,11 @@
+const today = new Date()
+
+const person = {
+  age: 20
+}
+
+class Color {
+
+}
+
+const red = new Color()
